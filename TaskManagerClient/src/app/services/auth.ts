1c06cd23746c06f83bc5bdca20e9a98302350c8d
adikrakover <ak0556745911@gmail.com>
2026-02-12 00:53:30 +0200

    Fix API URL

--- TaskManagerClient/src/app/services/auth.ts
+++ TaskManagerClient/src/app/services/auth.ts
@@ -2,14 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-// במקום להשתמש ב-environment, נכתוב את זה ישירות:
-private apiUrl = 'https://wolf-tasks-server-n3vx.onrender.com/api';
-  constructor(private http: HttpClient) {}
+ 
+  private apiUrl = environment.apiUrl; 
 
+  constructor(private http: HttpClient) {
+    console.log('API URL:', this.apiUrl);
+  }
   login(email: string, password: string): Observable<any> {
     return this.http.post(`${this.apiUrl}/auth/login`, { email, password });
   }
